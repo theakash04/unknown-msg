f7277e5c9604ffd08efdb6e89dc36d016f587c79
Akash kumar <Aakashkkr04@gmail.com>
2024-09-10 05:34:46 +0530

    [added]: Vercel speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import AuthProvider from "../context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
             &copy; 2024 unknown message. All rights reserved.
           </footer>
           <Analytics />
+          <SpeedInsights />
         </body>
       </AuthProvider>
     </html>
